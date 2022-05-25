47faae048c1ac51137ea9d99ed8eae02cba0c6d4
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-05-25 15:21:28 +0600

    deleted product done

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ async function run() {
       const result = await productCollection.insertOne(product);
       res.send(result);
     });
+    // delete product 
+    app.delete('/product/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(filter);
+      res.send(result);
+    })
     // load all users 
     app.get('/user', verifyJWT , async (req, res) => {
       const users = await userCollection.find().toArray();
